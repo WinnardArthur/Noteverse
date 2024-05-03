39ce51400c34d01843f009ea0729b14bb889eb52
WinnardArthur <winnardarthur7@gmail.com>
2024-05-03 09:07:49 +0000

    create hero section

---
+++ app/(root)/_components/heroes.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+export const Heroes = () => {
+  return (
+    <div className="flex flex-col items-center justify-center -mt-8">
+      <div className="flex flex-row-reverse items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+          <Image
+            src="/note-pic1.jpg"
+            fill
+            alt="document"
+            className="object-contain"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden md:block">
+          <Image
+            src="/note-pic3.jpg"
+            alt="notes"
+            fill
+            className="object-contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};

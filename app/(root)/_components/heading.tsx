b89c0ac74e839cc918470cfa0f6115b5037fd5b7
WinnardArthur <winnardarthur7@gmail.com>
2024-05-06 11:18:11 +0000

    complete auth for clerk and convex

--- app/(root)/_components/heading.tsx
+++ app/(root)/_components/heading.tsx
@@ -1,14 +1,22 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
 import React from "react";
+import { useConvexAuth } from "convex/react";
+import { ArrowRight } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Streamline your thoughts, stay organized and bring your ideas to life with
+        Streamline your thoughts, stay organized and bring your ideas to life
+        with
         <p>
           <span className="underline">Noteverse</span>
         </p>
@@ -17,9 +25,25 @@ export const Heading = () => {
         Simplify note-taking and task management <br />
         all in one place.
       </h3>
-      <Button>
-        Enter Noteverse <ArrowRight className="ml-2 w-4 h-4" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size={"lg"} />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href={"/documents"}>
+            Enter Noteverse <ArrowRight className="ml-2 w-4 h-4" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+          Get Noteverse free <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
--- app/(root)/_components/navbar.tsx
+++ app/(root)/_components/navbar.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { useScrollTop } from "@/hooks/use-scroll-top";
-import { cn } from "@/lib/utils";
 import React from "react";
+import { useConvexAuth } from "convex/react";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+
+import { cn } from "@/lib/utils";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -19,6 +26,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal" fallbackRedirectUrl="/">
+              <Button variant="ghost" size={"sm"}>
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size={"sm"}>Get Noteverse free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} asChild size={"sm"}>
+              <Link href="/documents">Enter Noteverse</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>

4471f891c759fd4336c974da34a5c66be2f965c3
WinnardArthur <winnardarthur7@gmail.com>
2024-05-06 09:05:45 +0000

    create footer

--- app/(root)/_components/footer.tsx
+++ app/(root)/_components/footer.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 z-50 bg-background">
+    <div className="flex items-center w-full p-6 z-50 bg-background dark:bg-[#1F1F1F] ">
       <Logo />
       <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
         <Button variant="ghost" size="sm">

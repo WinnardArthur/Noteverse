2d7852ebd5a300f2ccec2354c16e4e8a9b7af88f
WinnardArthur <winnardarthur7@gmail.com>
2024-05-09 17:14:23 +0000

    create trash feature apis

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,61 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
+// Get documents on sidebar
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticateed");
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user_parent", (q) =>
+        q.eq("userId", userId).eq("parentDocument", args.parentDocument)
+      )
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
+
+    return documents;
+  },
+});
+
+// Create a new document
+export const create = mutation({
+  args: {
+    title: v.string(),
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const document = await ctx.db.insert("documents", {
+      title: args.title,
+      parentDocument: args.parentDocument,
+      userId,
+      isArchived: false,
+      isPublished: false,
+    });
+
+    return document;
+  },
+});
+
+// Archive a document
 export const archive = mutation({
   args: { id: v.id("documents") },
   handler: async (ctx, args) => {
@@ -54,26 +109,21 @@ export const archive = mutation({
   },
 });
 
-// Get documents on sidebar
-export const getSidebar = query({
-  args: {
-    parentDocument: v.optional(v.id("documents")),
-  },
-  handler: async (ctx, args) => {
+// Get Documents in trash
+export const getTrash = query({
+  handler: async (ctx) => {
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
-      throw new Error("Not authenticateed");
+      throw new Error("Not authenticated");
     }
 
     const userId = identity.subject;
 
     const documents = await ctx.db
       .query("documents")
-      .withIndex("by_user_parent", (q) =>
-        q.eq("userId", userId).eq("parentDocument", args.parentDocument)
-      )
-      .filter((q) => q.eq(q.field("isArchived"), false))
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArchived"), true))
       .order("desc")
       .collect();
 
@@ -81,12 +131,9 @@ export const getSidebar = query({
   },
 });
 
-// Create a new document
-export const create = mutation({
-  args: {
-    title: v.string(),
-    parentDocument: v.optional(v.id("documents")),
-  },
+// Restore documents from trash
+export const restore = mutation({
+  args: { id: v.id("documents") },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
@@ -96,13 +143,72 @@ export const create = mutation({
 
     const userId = identity.subject;
 
-    const document = await ctx.db.insert("documents", {
-      title: args.title,
-      parentDocument: args.parentDocument,
-      userId,
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const recursiveRestore = async (documentId: Id<"documents">) => {
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) => {
+          return q.eq("userId", userId).eq("parentDocument", documentId);
+        })
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, { isArchived: false });
+        await recursiveRestore(child._id);
+      }
+    };
+
+    const options: Partial<Doc<"documents">> = {
       isArchived: false,
-      isPublished: false,
-    });
+    };
+
+    if (existingDocument.parentDocument) {
+      const parent = await ctx.db.get(existingDocument.parentDocument);
+      if (parent?.isArchived) {
+        options.parentDocument = undefined;
+      }
+    }
+
+    const updatedDocument = await ctx.db.patch(args.id, options);
+
+    recursiveRestore(args.id);
+
+    return updatedDocument;
+  },
+});
+
+// Remove document permanently
+export const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Document not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const document = await ctx.db.delete(args.id);
 
     return document;
   },

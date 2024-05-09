64c46a7b84a91419f5c40ac528eb0c3fc78f48ae
WinnardArthur <winnardarthur7@gmail.com>
2024-05-09 12:36:13 +0000

    create archive document feature

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,20 +2,29 @@
 
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
-import { ChevronLeft, MenuIcon } from "lucide-react";
+import {
+  ChevronLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
+import { useQuery, useMutation } from "convex/react";
 
 import { cn } from "@/lib/utils";
+import { api } from "@/convex/_generated/api";
 
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const pathname = usePathname();
 
-  const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -110,6 +119,16 @@ export const Navigation = () => {
     }
   };
 
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created",
+      error: "Failed to create a new note",
+    });
+  };
+
   return (
     <>
       <aside
@@ -132,11 +151,12 @@ export const Navigation = () => {
         </div>
         <div className="w-full">
           <UserItem />
+          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={() => {}} />
+          <Item label="New page" onClick={handleCreate} icon={PlusCircle} />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => {
-            return <p key={document._id}>{document.title}</p>;
-          })}
+          <DocumentList />
         </div>
 
         <div
--- convex/documents.ts
+++ convex/documents.ts
@@ -3,20 +3,85 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
-export const get = query({
-  handler: async (ctx) => {
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
       throw new Error("Not authenticated");
     }
 
-    const documents = await ctx.db.query("documents").collect();
+    const userId = identity.subject;
+
+    // Find document to delete/archive
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Document not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized");
+    }
+
+    // Recursively archive documents
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      // Find the children of this parent document with ID as documentId
+      const children = await ctx.db
+        .query("documents")
+        .withIndex("by_user_parent", (q) =>
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )
+        .collect();
+
+      // Loop through all the children and for each child,
+      for (const child of children) {
+        // Update its archive field to true
+        await ctx.db.patch(child._id, { isArchived: true });
+        // Run the recursive function again using current's child Id
+        // In case current child becomes a parent to nested documents.
+        await recursiveArchive(child._id);
+      }
+    };
+
+    const document = await ctx.db.patch(args.id, { isArchived: true });
+
+    // Pass the parent's document id to the recursive function
+    // to archive all nested children
+    recursiveArchive(args.id);
+    return document;
+  },
+});
+
+// Get documents on sidebar
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id("documents")),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticateed");
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user_parent", (q) =>
+        q.eq("userId", userId).eq("parentDocument", args.parentDocument)
+      )
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
 
     return documents;
   },
 });
 
+// Create a new document
 export const create = mutation({
   args: {
     title: v.string(),

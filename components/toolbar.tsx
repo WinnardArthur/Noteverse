c0d815b1cef704626a83a82be05b84a863a1f28f
WinnardArthur <winnardarthur7@gmail.com>
2024-05-15 10:00:05 +0000

    create image uploader

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,6 +8,7 @@ import { ImageIcon, Smile, X } from "lucide-react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import TextareaAutoSize from "react-textarea-autosize";
+import { useCoverImageStore } from "@/hooks/use-cover-image";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -21,6 +22,8 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
 
   const update = useMutation(api.documents.update);
   const removeIcon = useMutation(api.documents.removeIcon);
+
+  const coverImage = useCoverImageStore();
 
   const enableInput = () => {
     if (preview) return;
@@ -98,7 +101,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
         )}
         {!initialData.coverImage && !preview && (
           <Button
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
